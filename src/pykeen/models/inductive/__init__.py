2bc4053900932315a53cd05ee893404225dfa73c
MohamedGouaouri <im_gouaouri@esi.dz>
2023-02-06 21:26:40 +0100

    non sense

--- src/pykeen/models/inductive/__init__.py
+++ src/pykeen/models/inductive/__init__.py
@@ -5,9 +5,13 @@
 from .base import InductiveERModel
 from .inductive_nodepiece import InductiveNodePiece
 from .inductive_nodepiece_gnn import InductiveNodePieceGNN
+from .inductive_own import InductiveOwn
+from .inductive_own_gnn import InductiveOwnGNN
 
 __all__ = [
     "InductiveERModel",
     "InductiveNodePiece",
     "InductiveNodePieceGNN",
+    "InductiveOwn",
+    "InductiveOwnGNN"
 ]
