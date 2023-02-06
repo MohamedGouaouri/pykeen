ab612aded18dd49c9be5126e572c29ae3003511c
MohamedGouaouri <im_gouaouri@esi.dz>
2023-02-06 21:24:19 +0100

    non sense

---
+++ src/pykeen/models/inductive/inductive_own.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""A wrapper which combines an interaction function with NodePiece entity representations."""
+
+import logging
+from typing import Any, Callable, ClassVar, Mapping, Optional
+from ...typing import Initializer
+
+import torch
+from class_resolver import Hint, HintOrType, OptionalKwargs
+
+from .base import InductiveERModel
+from ...constants import DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE
+from ...nn import (
+    DistMultInteraction,
+    Interaction,
+    NodePieceRepresentation,
+    SubsetRepresentation,
+    representation_resolver,
+)
+from ...nn.node_piece import RelationTokenizer
+from ...nn.init import xavier_uniform_
+from ...triples.triples_factory import CoreTriplesFactory
+
+__all__ = [
+    "InductiveNodePiece",
+]
+
+logger = logging.getLogger(__name__)
+
+
+class InductiveOwn(InductiveERModel):
+
+    hpo_default: ClassVar[Mapping[str, Any]] = dict(
+        embedding_dim=DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE,
+    )
+
+    def __init__(
+        self,
+        *,
+        triples_factory: CoreTriplesFactory,
+        inference_factory: CoreTriplesFactory,
+        embedding_dim: int = 64,
+        interaction: HintOrType[Interaction] = DistMultInteraction,
+        validation_factory: Optional[CoreTriplesFactory] = None,
+        test_factory: Optional[CoreTriplesFactory] = None,
+        entity_initializer: Hint[Initializer] = xavier_uniform_,
+        **kwargs,
+    ) -> None:
+        """
+        Initialize the model.
+
+        :param triples_factory:
+            the triples factory of training triples. Must have create_inverse_triples set to True.
+        :param inference_factory:
+            the triples factory of inference triples. Must have create_inverse_triples set to True.
+        :param validation_factory:
+            the triples factory of validation triples. Must have create_inverse_triples set to True.
+        :param test_factory:
+            the triples factory of testing triples. Must have create_inverse_triples set to True.
+        :param embedding_dim:
+            the embedding dimension. Only used if embedding_specification is not given.
+        :param relation_representations_kwargs:
+            the relation representation parameters
+        :param interaction:
+            the interaction module, or a hint for it.
+
+        :param kwargs:
+            additional keyword-based arguments passed to :meth:`ERModel.__init__`
+
+        :raises ValueError:
+            if the triples factory does not create inverse triples
+        """
+        if not triples_factory.create_inverse_triples:
+            raise ValueError(
+                "The provided triples factory does not create inverse triples. However, for the node piece "
+                "representations inverse relation representations are required.",
+            )
+
+        if validation_factory is None:
+            validation_factory = inference_factory
+
+        super().__init__(
+            triples_factory=triples_factory,
+            interaction=interaction,
+            entity_representations=None,
+            entity_representations_kwargs=dict(
+                shape=embedding_dim,
+                triples_factory=triples_factory,
+                # Modification here
+                initializer=entity_initializer
+            ),
+            validation_factory=validation_factory,
+            testing_factory=test_factory,
+            **kwargs,
+        )
